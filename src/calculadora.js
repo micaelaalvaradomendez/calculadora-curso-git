76f16d349bfc5c55ba9871b977848b91969f1d50
Abril <abrilbenitezsmith@gmail.com>
2025-10-08 23:14:57 -0300

    Implemento Dividir

--- src/calculadora.js
+++ src/calculadora.js
@@ -12,7 +12,10 @@ class Calculadora {
   }
 
   dividir(a, b) {
-    // TODO: Implementar división
+        if (b === 0){
+        throw new Error("No se puede dividir por cero");
+        }
+        return a/b;
   }
 
   potencia(base, exponente) {
